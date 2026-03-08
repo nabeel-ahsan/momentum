d97d4017f23f2bf5caa9a73fc3ed57a26f5218eb
Nabeel <itsme007nabeel@gmail.com>
2026-03-08 20:11:25 +0530

    feat: refactor server setup by moving server logic to server.js and updating start script

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,12 @@
-import "dotenv/config";
-import { dbConnection } from "./src/config/db.js";
 import express from "express";
 import cors from "cors";
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-dbConnection();
+const app = express();
+
 app.use(cors());
 
 app.get("/health", (req, res) => {
   res.send("OK");
 });
 
-app.listen(PORT, () => {
-  console.log("Server is running");
-});
+export default app;
