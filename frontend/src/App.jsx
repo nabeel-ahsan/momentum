6ebfa5f6688391bdbe270a700a32d8394c2bd51a
Nabeel <itsme007nabeel@gmail.com>
2026-04-23 12:14:42 +0530

    feat: implement user registration form with validation and success/error messages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListSessions from "./pages/ListSessions";
 import LogIn from "./pages/LogIn";
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,158 @@
-import React from 'react'
+import { useState } from "react";
 
 const Register = () => {
-  return (
-    <div>Hello!</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Register
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+
+  const postData = async () => {
+    const url = "http://localhost:3000/auth/signup";
+    try{
+    const response = await fetch(url, {
+      method: "POST",
+
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }),
+
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const data = await response.json();
+    if(response.ok){
+      setSubmitted(true)
+      setError(false)
+      setName("")
+      setEmail("")
+      setPassword("")
+    }
+    else {
+      alert(data.message || "Error creating user!")
+    }
+    }
+    catch(error){
+      console.error("Error: ", error);
+      alert("Error occured!")
+    }
+  };
+
+  const handleName = (e) => {
+    setName(e.target.value);
+    setSubmitted(false);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setSubmitted(false);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setSubmitted(false);
+  };
+
+  const validateForm = () => {
+    if (name === "" || email === "" || password === "") {
+      setError(true);
+
+      return false;
+    } else if (!email.includes("@")) {
+      setError(true);
+
+      return false;
+    } else if (password.length < 6) {
+      setError(true);
+
+      return false;
+    } else {
+      setError(false);
+      return true;
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      await postData();
+    } else {
+      errorMessage();
+    }
+  };
+
+  const successMessage = () => {
+    return (
+      <div
+        className="success"
+        style={{
+          display: submitted ? "" : "none",
+        }}
+      >
+        <h1>User {name} successfully registered!!</h1>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div
+        className="error"
+        style={{
+          display: error ? "" : "none",
+        }}
+      >
+        <h1>Please enter all the fields</h1>
+      </div>
+    );
+  };
+
+  return (
+    <div className="form">
+      <div>
+        <h1>User Registration</h1>
+      </div>
+
+      <div className="messages">
+        {errorMessage()}
+        {successMessage()}
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label className="label">Name</label>
+        <input
+          onChange={handleName}
+          className="input"
+          value={name}
+          type="text"
+        />
+
+        <label className="label">Email</label>
+        <input
+          onChange={handleEmail}
+          className="input"
+          value={email}
+          type="email"
+        />
+
+        <label className="label">Password</label>
+        <input
+          onChange={handlePassword}
+          className="input"
+          value={password}
+          type="password"
+        />
+
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
